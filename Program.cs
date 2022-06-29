8c75a2dc4d0fb9603f79beb471640228814d5b0d
Caleb Wolfert <calebwolfert@gmail.com>
2022-06-29 11:01:33 -0600

    genie

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //Caleb input test
-<<<<<<< HEAD
 //PY input test
-=======
 //James input test
->>>>>>> 389a19a6a2f1395d36e945793a7669ec72210d98
 
