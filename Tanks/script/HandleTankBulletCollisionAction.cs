e09963f07d7ac8481f4c4e36ac62bb9fa84b57a8
bbranham4162 <brysonbranham@gmail.com>
2022-07-11 12:46:52 -0600

    Made tanks disappear when hit by bullets.

--- Tanks/script/HandleTankBulletCollisionAction.cs
+++ Tanks/script/HandleTankBulletCollisionAction.cs
@@ -26,21 +26,38 @@ namespace Tanks.script {
         }
 
         public override void execute(Cast cast, Script script, Clock clock, Callback callback) {
-            // Grab the ship from the cast
-            this.tank1 = cast.GetFirstActor("tank1");
-            this.tank2 = cast.GetFirstActor("tank2");
+            // Grab the tank from the cast
+            this.tank1 = cast.GetFirstActor("Tank1");
+            this.tank2 = cast.GetFirstActor("Tank2");
 
             // Only worry about collision if the ship actually exists
             if (this.tank2 != null) {
                 foreach (Actor bullet1 in cast.GetActors("bullet1")) {
                     if (this.physicsService.CheckCollision(this.tank2, bullet1)) {
-                        cast.RemoveActor("tank2", tank2);
+                        
+                        Console.WriteLine("I hit a Tank!!!!!");
+
+                        cast.RemoveActor("Tank2", tank2);
                         cast.RemoveActor("bullet1", bullet1);
                         // this.audioService.PlaySound("asteroid/assets/sound/explosion-01.wav", (float) 0.1);
                         
                     }
                 }
             }
+
+             if (this.tank1 != null) {
+                foreach (Actor bullet2 in cast.GetActors("bullet2")) {
+                    if (this.physicsService.CheckCollision(this.tank1, bullet2)) {
+                        
+                        Console.WriteLine("I hit a Tank!!!!!");
+
+                        cast.RemoveActor("Tank1", tank1);
+                        cast.RemoveActor("bullet2", bullet2);
+                        // this.audioService.PlaySound("asteroid/assets/sound/explosion-01.wav", (float) 0.1);
+                        
+                    }
+                }
+             }
 
             // if (this.tank != null) {
             //     foreach (Actor bullet in cast.GetActors("bullet2")) {
