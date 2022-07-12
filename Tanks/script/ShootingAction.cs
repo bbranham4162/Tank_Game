693288985b62d566e55bcaff446541ffb497e1a3
Caleb Wolfert <calebwolfert@gmail.com>
2022-07-12 10:27:30 -0600

    shooting hopeful fix?

--- Tanks/script/ShootingAction.cs
+++ Tanks/script/ShootingAction.cs
@@ -49,62 +49,16 @@ namespace genie.script{
                 float bulletX = tank.GetX();
                 float bulletY = tank.GetY() - (tank.GetHeight()/2);
 
-                //xVelocity^2 + yVelocity^2 = velocity^2
-
-                // Create the bullet and put it in the cast
+                // speed of the bullet
                 float velocity = 10;
 
                 // sets up x and y velocities for the next step
                 double xVelocity = 0;
                 double yVelocity = 0;
 
-                
-
-                //Chooses one of the four quadrants to fire the bullet into
-                if(tank.GetRotation() == 0) {
-                    xVelocity = 0;
-                    yVelocity = -velocity;
-                }
-                else if (tank.GetRotation() > 0 && tank.GetRotation() < 90) {
-                    // top right
-                    xVelocity = velocity;
-                    yVelocity = -velocity;
-                }
-                else if(tank.GetRotation() == 90) {
-                    xVelocity = velocity;
-                    yVelocity = 0;
-                }
-                else if (tank.GetRotation() > 90 && tank.GetRotation() < 180) {
-                    //bottom right
-                    xVelocity = velocity;
-                    yVelocity = velocity;
-                }
-                else if(tank.GetRotation() == 180) {
-                    xVelocity = 0;
-                    yVelocity = velocity;
-                }
-                else if (tank.GetRotation() > 180 && tank.GetRotation() < 270) {
-                    //bottom left
-                    xVelocity = -velocity;
-                    yVelocity = velocity;
-                }
-                else if(tank.GetRotation() == 270) {
-                    xVelocity = -velocity;
-                    yVelocity = 0;
-                }
-                else {
-                    //top left
-                    xVelocity = -velocity;
-                    yVelocity = -velocity;
-                }
-
                 double radians = (tank.GetRotation() * Math.PI) / 180;
-                double slopeDec = Math.Tan(radians);
-                xVelocity = slopeDec * 10;
-                yVelocity = (1 - slopeDec) * 10;
-
-                bulletVel.vx = (float)xVelocity;
-                bulletVel.vy = -(float)yVelocity;
+                yVelocity = -(float)(velocity * Math.Cos(radians));
+                xVelocity = (float)(velocity * Math.Sin(radians));
 
                 Actor bullet = new Actor("Tanks/assets/Cannonball/cannon ball_1.png", 20, 20, bulletX, bulletY, bulletVel.vx, bulletVel.vy);
                 cast.AddActor("bullets", bullet);
